ec512b1c2336818ef58cd928337da08d627a10b0
Abhishek20042003 <141939848+Abhishek20042003@users.noreply.github.com>
2025-10-08 17:43:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
-let all = document.querySelectorAll('.square')
-all.forEach.((box)=>{
-	box.addEventListener("mouseover", function handler() {
-		let box1 = document.getElementById('square1');
-		let box1 = document.getElementById('square2');
-		let box1 = document.getElementById('square3');
+let all = document.querySelectorAll('.square');
+all.forEach((box) => {
+    box.addEventListener("mouseover", function handler() {
+        all.forEach(b => {
+            if (b.id !== this.id) {
+                b.style.backgroundColor = "#6F4E37";
+            }
+        });
+    });
 
-		for(let box of all){
-			if(box.id != this.id){
-				box.style.backgroundcolor = "#6F4E37";
-			}
-		}
-			box.addEventListener("mouseout", function handler(){
-		all.forEach(box=>{
-			box.style.backgroundcolor = "#E6E6FA";
-			
-		})
-		
-	});
-	});
-
+    box.addEventListener("mouseout", function handler() {
+        all.forEach(b => {
+            b.style.backgroundColor = "#E6E6FA";
+        });
+    });
+});
 	
