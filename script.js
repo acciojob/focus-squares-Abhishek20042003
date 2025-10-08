8c2000394039a6f2b2d164235459d8c586cc9e23
Abhishek20042003 <141939848+Abhishek20042003@users.noreply.github.com>
2025-10-08 17:37:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 let all = document.querySelectorAll('.square')
 all.forEach.((box)=>{
 	box.addEventListener("mouseover", function handler() {
-		let box1 = document.getElementById('square1')
-		let box1 = document.getElementById('square2')
-		let box1 = document.getElementById('square3')
+		let box1 = document.getElementById('square1');
+		let box1 = document.getElementById('square2');
+		let box1 = document.getElementById('square3');
 
 		for(let box of all){
 			if(box.id != this.id){
 				box.style.backgroundcolor = "#6F4E37";
 			}
 		}
-		
-	});
-
-		box.addEventListener("mouseout", function handler(){
+			box.addEventListener("mouseout", function handler(){
 		all.forEach(box=>{
-			box.style.backgroundcolor = '#E6E6FA'
+			box.style.backgroundcolor = "#E6E6FA";
 			
 		})
 		
 	});
+	});
+
+	
